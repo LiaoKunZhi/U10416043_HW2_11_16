3923321b351f8207df5eb4c0b6d8978923674d83
LiaoKunZhi <a0956823675@gmail.com>
2016-03-07 20:17:34 +0800

    Create AnswerList.java
    First,let the data fields which from TestRepeatMultiplicationQuiz.java can be show in this new class.And create a list to save the number which user enter.
    Second,make a constructor, three get methods, and three set methods to make sure the three data fields can be work in this class.

--- AnswerList.java
+++ AnswerList.java
@@ -16,5 +16,36 @@ class AnswerList{
 	ArrayList<Integer> answerList = new ArrayList<>();
 
 	Scanner input = new Scanner(System.in);
-	
+
+	//Constructor
+	AnswerList(int newNumber1, int newNumber2, int newAnswer){
+		number1 = newNumber1;
+		number2 = newNumber2;
+		answer = newAnswer;
+	}
+
+	//Get method and set method
+	void setNumber1(int newNumber1) {
+		number1 = newNumber1;
+	}
+
+	int getNumber1() {
+		return number1;
+	}
+
+	void setNumber2(int newNumber2) {
+		number2 = newNumber2;
+	}
+
+	int getNumber2() {
+		return number2;
+	}
+
+	void setAnswer(int newAnswer) {
+		answer = newAnswer;
+	}
+
+	int getAnswer() {
+		return answer;
+	}
 }
