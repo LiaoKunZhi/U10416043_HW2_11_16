6de202e60030969f031e4c145308dbcfa806d909
LiaoKunZhi <a0956823675@gmail.com>
2016-03-07 19:39:38 +0800

    Create TestRepeatAdditionQuiz.java-1
    First,make a class to keep in other code.And create two variables to make the question.

---
+++ TestRepeatAdditionQuiz.java
@@ -0,0 +1,14 @@
+//U10416043   廖崑智
+
+import java.util.Scanner;
+
+//Class
+public class TestRepeatAdditionQuiz{
+
+	//Main method
+	public static void main(String[] args){
+		int number1 = (int)(Math.random() * 10);
+		int number2 = (int)(Math.random() * 10);
+
+	}
+}
